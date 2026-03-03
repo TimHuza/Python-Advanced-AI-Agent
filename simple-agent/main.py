4b305481ad8c469ed5da7c5823eb53ed181b0ce2
Tim <tim.ca2023@gmail.com>
2026-03-02 20:48:14 -0800

    Finished simple agent

--- simple-agent/main.py
+++ simple-agent/main.py
@@ -1,4 +1,4 @@
-from mcp import ClientSession, StudioServerParameters
+from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
@@ -8,3 +8,55 @@ import asyncio
 import os
 
 load_dotenv()
+
+model = ChatOllama(
+    model="llama3.2:1b",
+    temperature=0,
+    ollama_api_url="http://localhost:11434"
+)
+
+server_params = StdioServerParameters(
+    command="npx",
+    env={
+        "FIRECRAWL_API_KEY": os.getenv("FIRECRAWL_API_KEY"),
+    },
+    args=["firecrawl-mcp"]
+)
+
+
+async def main():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+            tools = await load_mcp_tools(session)
+            agent = create_react_agent(model, tools)
+
+            messages = [
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant that can scrape websites, crawl pages, and extract data using Firecrawl tools. Think step by step and use the appropriate tools to help the user."
+                }
+            ]
+
+            print("Available Tools -", *[tool.name for tool in tools]) # * will unpack all of these values into individual arguments
+            print("-" * 60)
+
+            while True:
+                user_input = input("\nYou: ")
+                if user_input.lower == "/bye":
+                    print("Goodbye!")
+                    break
+
+                messages.append({"role": "user", "content": user_input[:175000]})
+
+                try:
+                    agent_response = await agent.ainvoke({"messages": messages})
+
+                    ai_message = agent_response["messages"][-1].content
+                    print("\nAgent:", ai_message)
+                except Exception as e:
+                    print("Error:", e)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
